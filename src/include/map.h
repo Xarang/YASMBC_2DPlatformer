72b2595e5ecaa619d59acd9207588fb255d3d5bc
Victor Coatalem <victor.coatalem@epita.fr>
2018-11-09 22:42:11 +0100

    map structure updated

--- src/include/map.h
+++ src/include/map.h
@@ -1,6 +1,21 @@
 #ifndef MAP_H_
 #define MAP_H_
 
+struct map
+{
+    size_t width;
+    size_t height;
+    char *blocks;
+    struct vector2 start;
+}
 
+enum block_type
+{
+    VOID,
+    BLOCK,
+    FINISH
+};
+
+struct map *load_map(const char *filename);
 
 #endif /*MAP_H_*/
