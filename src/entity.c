b454623ef17c7c03ea9cfe0d739dbf5d99b50dfc
francois.te <francois.te@epita.fr>
2018-11-10 00:15:37 +0100

    [ADD] Added entity.c, update.c and update.h

---
+++ src/entity.c
@@ -0,0 +1,9 @@
+#include "entity.h"
+
+void move_player(struct entity *player, enum action action)
+{
+    switch (action)
+    {
+    case LEFT
+    }
+}
---
+++ src/include/update.h
@@ -0,0 +1,8 @@
+#ifndef UPDATE_H
+#define UPDATE_H
+
+#include "gamestate.h"
+
+void update(struct gamestate *gamestate);
+
+#endif /* ! UPDATE_H */
