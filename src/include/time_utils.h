587b1a79fa0ca7b73109cd0b05b256a8b2be8e0e
francois.te <francois.te@epita.fr>
2018-11-09 23:43:00 +0100

    [ADD] Added time_utils header and c file

---
+++ src/include/time_utils.h
@@ -0,0 +1,6 @@
+#ifndef TIME_UTILS_H
+#define TIME_UTILS_H
+
+double delta_time(uint64_t *last_update_time);
+
+#endif /* ! TIME_UTILS_H */
---
+++ src/time_utils.c
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+//#include "time_utils.h"
+
+double delta_time(uint64_t *last_update_time)
+{
+    double frequency = SDL_GetPerformanceFrequency();
+
+    uint64_t last = *last_update_time;
+    uint64_t now = SDL_GetPerformanceCounter();
+
+    *last_update_time = now;
+
+    return (last - now) * 1000 / frequency;
+}
