b47b506d1d25c504b244897b19231c4104712d49
francois.te <francois.te@epita.fr>
2018-11-10 05:32:57 +0100

    [ADD] Added untested feature to player : Cannot go through blocks

--- src/player.c
+++ src/player.c
@@ -4,7 +4,8 @@
 #define PLAYER_G_FORCE (-10)
 #define PLAYER_MAX_VEL 0.01
 
-void update_player(struct entity *entity, struct gamestate *gamestate)
+static struct transform get_new_transform(struct entity *player,
+                                          struct gamestate *gamestate)
 {
     struct vector2 acc = { 0, -PLAYER_G_FORCE };
     double delta = delta_time(&gamestate->last_update_time);
@@ -17,4 +18,29 @@ void update_player(struct entity *entity, struct gamestate *gamestate)
         tf.vel = vector2_scale(tf.vel, PLAYER_MAX_VEL / vel_norm);
     }
     tf.pos = vector2.add(tf.pos, tf.vel, delta);
+
+}
+
+void update_player(struct entity *player, struct gamestate *gamestate)
+{
+    struct transform new_tf = get_new_transform(player, gamestate);
+    struct transform old_tf = player->transform;
+
+    //If the new vertical position is in a block
+    if (map_get_type(gamestate->map, old_tf.pos.x, new_tf.pos.y) == BLOCK)
+    {
+        //If the new vertical position is lower
+        if (new_tf.pos.y >= old_tf.posx)
+        {
+            player->is_grounded = 1;
+        }
+        neW_tf.vel.y = 0.0;
+        new_tf.pos.y = old_tf.pos.y;
+    }
+    //If the new horizontal position is in a block
+    if (map_get_type(famestate->map, new_tf.pos.x, old_tf.y) == BLOCK)
+    {
+        new_tf.vel.x = 0.0;
+        new_tf.pos.x = old_tf.pos.x;
+    }
 }
