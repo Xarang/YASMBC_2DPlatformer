f4bea1890db15bab1c6a263c477fce110ab5216e
francois.te <francois.te@epita.fr>
2018-11-11 07:01:58 +0100

    [MISC] Temporarly put back the Macros for foe_1 velocity

--- src/include/foe_1.h
+++ src/include/foe_1.h
@@ -3,6 +3,9 @@
 
 #include "entity.h"
 
+#define FOE_1_X_VEL 0.001
+#define FOE_1_Y_VEL 0.0
+
 enum entity_status update_foe_1(struct entity *foe,
                                 struct gamestate *gamestate);
 int collides_foe_1(struct entity *player, struct entity *foe);
